b38f2757bfc7dd4bde94d4375ad575053028d65b
Abdulaziz Aleissa <aleissa220@gmail.com>
2024-11-01 02:58:01 +0300

    v0.02 Fixed Major Errors

--- app/run.py
+++ app/run.py
@@ -1,21 +1,39 @@
 import json
 import plotly
 import pandas as pd
+from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
+from nltk.tokenize import word_tokenize, sent_tokenize
 from flask import Flask, render_template, request, jsonify
 from plotly.graph_objs import Bar
 from joblib import load
+import nltk
 from sqlalchemy import create_engine
+from sklearn.base import BaseEstimator, TransformerMixin
 
 app = Flask(__name__)
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    def starting_verb(self, text):
+        sentence_list = sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = pos_tag(word_tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return 1
+        return 0
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 # Tokenization function
 def tokenize(text):
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
-
     clean_tokens = [lemmatizer.lemmatize(tok).lower().strip() for tok in tokens]
     return clean_tokens
 
@@ -30,9 +48,13 @@ model = load("models/classifier.pkl")
 @app.route('/')
 @app.route('/index')
 def index():
-    # Data extraction for visuals
+    # Data extraction for the first visualization
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+
+    # Data extraction for the second visualization (top 10 categories)
+    category_counts = df.iloc[:, 4:].sum().sort_values(ascending=False).head(10)
+    category_names = list(category_counts.index)
 
     # Visualizations
     graphs = [
@@ -47,6 +69,19 @@ def index():
                 'title': 'Distribution of Message Genres',
                 'yaxis': {'title': "Count"},
                 'xaxis': {'title': "Genre"}
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+            'layout': {
+                'title': 'Top 10 Message Categories',
+                'yaxis': {'title': "Count"},
+                'xaxis': {'title': "Category"}
             }
         }
     ]
